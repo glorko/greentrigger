23356bc8f4fbf0bfce4427c5b162f4940605a0d1
Aleksandr_Kichev <kichev.alexander@gmail.com>
2024-10-10 13:07:50 +0800

    On-demand starter POC - in progress [3]
    - HAProxy with Data plane API is executed
    - Need to try to use it to change config in real time

--- resources/haproxy/haproxy-run.sh
+++ resources/haproxy/haproxy-run.sh
@@ -1,7 +1,10 @@
-docker run -d --name my-haproxy \
-    -v $(pwd)/resources/haproxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg \
-    -p 80:80 \
-    -p 8404:8404 \
-    haproxy:latest
+if (-Not (Test-Path "./haproxy-run")) { mkdir haproxy-run }; cp -r ./resources/haproxy/* ./haproxy-run
 
-#
+
+docker run -d --name my-haproxy `
+    -v "${PWD}/haproxy-run:/usr/local/etc/haproxy:rw" `
+    -p 80:80 `
+    -p 8404:8404 `
+    -p 5555:5555 `
+    haproxytech/haproxy-ubuntu
+
