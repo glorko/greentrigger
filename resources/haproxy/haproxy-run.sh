807db93f91432be5ae5f27b77fd815862c4fef91
Aleksandr Kichev <kichev.alexander@gmail.com>
2024-11-05 15:19:59 +0800

    HAProxy working directory now ignored by go

--- resources/haproxy/haproxy-run.sh
+++ resources/haproxy/haproxy-run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Create the haproxy-run directory if it doesn't exist
-if [ ! -d "./haproxy-run" ]; then
-    mkdir haproxy-run
+if [ ! -d "./.haproxy-run" ]; then
+    mkdir .haproxy-run
 fi
 
 # Copy the contents of the resources/haproxy directory to the haproxy-run directory
-cp -r ./resources/haproxy/* ./haproxy-run
+cp -r ./resources/haproxy/* ./.haproxy-run
 
 # Run the Docker container with HAProxy
 docker run -d --name my-haproxy \
     --network=host \
-    -v "${PWD}/haproxy-run:/usr/local/etc/haproxy:rw" \
+    -v "${PWD}/.haproxy-run:/usr/local/etc/haproxy:rw" \
     haproxytech/haproxy-ubuntu
